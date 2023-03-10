5d05cde38f110a89e3bc926b7eb176aaa1f6006b
amn-xyz <amaan2@ualberta.ca>
2023-03-09 18:24:32 -0700

    has city test will pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -54,8 +54,7 @@ public class CustomList extends ArrayAdapter<City> {
 
     }
     public boolean hasCity(City city){
-        //return cities.contains(city);
-        return false;
+        return cities.contains(city);
     }
 
 
